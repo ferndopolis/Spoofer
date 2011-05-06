20dcf38a7dc2aa8efcdbf66cbcad5d4177999708
Martin <mfiero@nettles.(none)>
2011-05-05 21:14:53 -0700

    Added Thread.new class method to run firefox as separate process.

--- visit_spoofer.rb
+++ visit_spoofer.rb
@@ -1,27 +1,33 @@
 #script for running anonymous hits to website for testing sid and pid information
 
 #input the values
-#pids = {
-#  500 => [{:sid => 12, :requests => 5},
-#          {:sid => 15, :requests => 2},
-#          {:sid => 17, :requests => 20}],
-#  501 => [{:sid => 9, :requests => 5},
-#          {:sid => 30, :requests => 20}],
-#  505 => [{:sid => 39, :requests => 18}]
-#}
+#
+=begin
+pids = {
+  500 => [{:sid => 12, :requests => 5},
+          {:sid => 15, :requests => 2},
+          {:sid => 17, :requests => 20}],
+  501 => [{:sid => 9, :requests => 5},
+          {:sid => 30, :requests => 20}],
+  505 => [{:sid => 39, :requests => 18}]
+}
+=end
 
 def make_ps_req(website,pids)
   #access tor control to change circuit 
   exec_tor_control='./tor_new_circuit.sh'
+  prequest=0
   pids.each do |pid, sr_list|
     sr_list.each do |req|
       req[:requests].times do
+        Thread.new {`torify firefox "http://#{website}?pid=#{pid}&sid=#{req[:sid]}" &`}
+        #Thread.new {`torify firefox "http://www.whatismyip.com" &`}
+        sleep 75 
+        prequest=prequest+1
+        puts "You are on request number #{prequest}."
         puts `#{exec_tor_control}`
-        sleep 15
-        puts `torify firefox "http://#{website}?pid=#{pid}&sid=#{req[:sid]}"`
-      end   
-    puts `pkill firefox-bin` 
+        puts `pkill firefox-bin`
+      end  
     end
   end
 end
-
